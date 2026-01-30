ce890ee3d666766b295f64c96b1a09215053bba1
Prajwal <prajwal9625@gmail.com>
2026-01-30 16:23:06 +0530

    Add files via upload

---
+++ ch_03practicesetQ2.py
@@ -0,0 +1,12 @@
+# write a program to fill in the template given by below with name and data
+#name="ninja",date=32 december 2029
+
+
+letter='''
+dear <name>
+hi <intro>
+welcome <home>
+'''
+
+print(letter.replace("<name>","prajwal").replace("<intro>","google") .replace("<home>" , "mane"))
+print(letter.replace("dear","hello magesh"))
---
+++ escapesequences.py
@@ -0,0 +1,84 @@
+#using escape sequences 
+
+a="prajwal is a good boy\nnot a bad boy"                #\n (backslash n used for new line )
+b="im coderand \nim also be programmer"
+c="im coderand \tim also be programmer"                 #\t used for the space bw two lines
+
+print(a)
+print(b)
+print(c)
+
+
+print("How was That :")
+print("name    \t marks")
+print("prajwal \t 89")
+print("harry   \t 90")
+
+
+'''
+Here are simple Python programs using escape sequences.
+
+✅ Program 1: New Line (\n)
+print("Welcome\nPython\nProgramming")
+
+
+Output:
+
+Welcome
+Python
+Programming
+
+✅ Program 2: Tab Space (\t)
+print("Name\tMarks")
+print("Ram\t85")
+print("Sita\t90")
+
+
+Output:
+
+Name    Marks
+Ram     85
+Sita    90
+
+✅ Program 3: Print Quotes (\' and \")
+print('It\'s a Python class')
+print("He said \"Python is easy\"")
+
+
+Output:
+
+It's a Python class
+He said "Python is easy"
+
+✅ Program 4: Backslash (\\)
+print("File path is C:\\Python\\Programs")
+
+
+Output:
+
+File path is C:\Python\Programs
+
+✅ Program 5: Backspace (\b)
+print("Helloo\b")
+
+
+Output:
+
+Hello
+
+✅ Program 6: Carriage Return (\r)
+print("Hello\rWorld")
+
+
+Output:
+
+World
+
+✅ Program 7: Raw String (Ignore escape sequences)
+print(r"C:\new\test\file")
+
+
+Output:
+
+C:\new\test\file
+'''
